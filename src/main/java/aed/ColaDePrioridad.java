695c90d3f8ee229f05d81591429728f4dc4b7790
Santino Ferrazzuolo <santinoferrazzuolo@proton.me>
2024-11-12 20:04:30 -0300

    formateado

--- src/main/java/aed/ColaDePrioridad.java
+++ src/main/java/aed/ColaDePrioridad.java
@@ -7,7 +7,6 @@ public class ColaDePrioridad<T> {
     private ArrayList<T> elementos;
     private int longitud;
     private Comparator<T> comparador;
-     
 
     public ColaDePrioridad(Comparator<T> c) {
         elementos = new ArrayList<T>();
@@ -22,25 +21,24 @@ public class ColaDePrioridad<T> {
     public void Encolar(T e) {
 
         elementos.add(e);
-        longitud += 1; 
-        
-        if(longitud == 1) {
+        longitud += 1;
+
+        if (longitud == 1) {
 
             return;
         }
-
         int i = longitud - 1;
-        
-        T padre = elementos.get((i-1)/2);
 
-        while (comparador.compare(e,padre) > 0 && i > 0){
-            elementos.set(i,padre); 
-            elementos.set((i-1)/2,e);
+        T padre = elementos.get((i - 1) / 2);
+
+        while (comparador.compare(e, padre) > 0 && i > 0) {
+            elementos.set(i, padre);
+            elementos.set((i - 1) / 2, e);
             i = (i - 1) / 2;
-            if (i > 0){
-                padre = elementos.get((i-1)/2);
+            if (i > 0) {
+                padre = elementos.get((i - 1) / 2);
             }
-            
+
         }
     }
 
@@ -48,54 +46,52 @@ public class ColaDePrioridad<T> {
         return elementos.get(0);
     }
 
-    public T DesencolarMax() {      //desencolando 
+    public T DesencolarMax() { // desencolando
         T res = elementos.get(0);
         longitud -= 1;
-        elementos.set(0,elementos.get(longitud));
+        elementos.set(0, elementos.get(longitud));
         elementos.remove(longitud);
         int i = 0;
-        T e = elementos.get(i); 
-        if ((2*i+1)<longitud) {
-        T  hijoIzq = elementos.get(2*i + 1);
-        }    
-        if ((2*i+2)<longitud){
-        T hijoDer = elementos.get(2*i + 2);
+        T e = elementos.get(i);
+        if ((2 * i + 1) < longitud) {
+            T hijoIzq = elementos.get(2 * i + 1);
+        }
+        if ((2 * i + 2) < longitud) {
+            T hijoDer = elementos.get(2 * i + 2);
         }
 
-         while ( i < longitud && (comparador.compare(e,hijoDer) < 0 || comparador.compare(e,hijoIzq) < 0) ) {
-            if (comparador.compare(hijoDer,hijoIzq) < 0){
-                elementos.set(i,hijoDer);
-                elementos.set(2*i + 2,e);
-                i = 2*i + 2;
+        while (i < longitud && (comparador.compare(e, hijoDer) < 0 || comparador.compare(e, hijoIzq) < 0)) {
+            if (comparador.compare(hijoDer, hijoIzq) < 0) {
+                elementos.set(i, hijoDer);
+                elementos.set(2 * i + 2, e);
+                i = 2 * i + 2;
             } else {
-                elementos.set(i,hijoIzq);
-                elementos.set(2*i + 1,e);
-                i = 2*i + 1;
+                elementos.set(i, hijoIzq);
+                elementos.set(2 * i + 1, e);
+                i = 2 * i + 1;
             }
             e = elementos.get(i);
-            boolean tieneHijoIzq = (2*i + 1) < longitud;
-            boolean tieneHijoDer = (2*i + 2) < longitud;
-            
-            if(tieneHijoIzq) {
-            hijoIzq = elementos.get(2*i + 1);
+            boolean tieneHijoIzq = (2 * i + 1) < longitud;
+            boolean tieneHijoDer = (2 * i + 2) < longitud;
+
+            if (tieneHijoIzq) {
+                hijoIzq = elementos.get(2 * i + 1);
             }
-            if(tieneHijoDer) {
-            hijoDer = elementos.get(2*i + 2);
+            if (tieneHijoDer) {
+                hijoDer = elementos.get(2 * i + 2);
             }
 
-            if(!tieneHijoDer && !tieneHijoIzq) {
+            if (!tieneHijoDer && !tieneHijoIzq) {
                 i = longitud;
             }
         }
-        return res; 
+        return res;
     }
 
-//proc ColaPrioridadVacía()
-//proc vacía()
-//proc encolar()
-//proc desencolarMax()
-// proc cambiarPrioridad()
-
-
+    // proc ColaPrioridadVacía()
+    // proc vacía()
+    // proc encolar()
+    // proc desencolarMax()
+    // proc cambiarPrioridad()
 
 }
